bc26b9f166bf26c28fd957b224c392a25ac1b685
Lumoshive Academy <lumoshive.academy@gmail.com>
2024-09-19 16:56:58 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import express from 'express'
 import cors from 'cors'
 
 import router from './router'
+require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 
 app.use(cors())
 
